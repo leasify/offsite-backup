7bc693db394462bd8c2777ec1d3f20e08ec0b62a
Andreas Ek <andreas.ek@leasify.se>
2024-04-22 18:51:01 +0200

    wip

--- backup.sh
+++ backup.sh
@@ -3,11 +3,11 @@
 # Leasify AB, Andreas Ek, 2024-04-22
 
 now=$(date +"%Y-%m-%d")
-mkdir -p /backup/${now}
-mkdir -p /backup/${now}/leasify-trunk-1
-mkdir -p /backup/${now}/leasify-backups
+sudo mkdir -p /backup/${now}
+sudo mkdir -p /backup/${now}/leasify-trunk-1
+sudo mkdir -p /backup/${now}/leasify-backups
 
-aws s3 sync s3://leasify-trunk-1 /backup/${now}/leasify-trunk-1
-aws s3 sync s3://leasify-backups /backup/${now}/leasify-backup
+sudo aws s3 sync s3://leasify-trunk-1 /backup/${now}/leasify-trunk-1
+sudo aws s3 sync s3://leasify-backups /backup/${now}/leasify-backup
 
-find /backup -type d -mtime +30 -exec rm -rf {} \;
+sudo find /backup -type d -mtime +30 -exec sudo rm -rf {} \;

9851b6c4d4b86c73d08a0345a433de99509c36b1
Andreas Ek <andreas.ek@leasify.se>
2024-05-14 10:00:40 +0200

    wip

--- backup.sh
+++ backup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 # file: backup.sh
-# Leasify AB, Andreas Ek, 2024-04-22
+# Leasify AB, Andreas Ek, 2024-05-14
 
-now=$(date +"%Y-%m-%d")
-sudo mkdir -p /backup/${now}
-sudo mkdir -p /backup/${now}/leasify-trunk-1
-sudo mkdir -p /backup/${now}/leasify-backups
-
-yday=$(date --date "yesterday" +"%Y-%m-%d")
+#yday=$(date --date "yesterday" +"%Y-%m-%d")
 #echo "${yday}"
 
-sudo cp -Rf /backup/${yday}/leasify-trunk-1 /backup/${now}/leasify-trunk-1
-sudo cp -Rf /backup/${yday}/leasify-backups /backup/${now}/leasify-backups
+latest=$(ls -td /backup/*/ | head -1)
 
-sudo aws s3 sync s3://leasify-trunk-1 /backup/${now}/leasify-trunk-1
-sudo aws s3 sync s3://leasify-backups /backup/${now}/leasify-backup
+echo "Latest backup: ${latest}"
 
-sudo find /backup -type d -mtime +30 -exec sudo rm -rf {} \;
+#now=$(date +"%Y-%m-%d")
+#sudo mkdir -p /backup/${now}
+#sudo mkdir -p /backup/${now}/leasify-trunk-1
+#sudo mkdir -p /backup/${now}/leasify-backups
+
+#sudo cp -Rf "`ls -dtr1 /backup/* | tail -1`" /backup/${now}/leasify-trunk-1
+#sudo cp -Rf "`ls -dtr1 /backup/* | tail -1`" /backup/${now}/leasify-trunk-1
+
+#sudo cp -Rf /backup/${yday}/leasify-trunk-1 /backup/${now}/leasify-trunk-1
+#sudo cp -Rf /backup/${yday}/leasify-backups /backup/${now}/leasify-backups
+
+#sudo aws s3 sync s3://leasify-trunk-1 /backup/${now}/leasify-trunk-1
+#sudo aws s3 sync s3://leasify-backups /backup/${now}/leasify-backup
+
+#sudo find /backup -type d -mtime +30 -exec sudo rm -rf {} \;

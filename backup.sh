c7b67d7d69b91b10143d47d49250d51b6f94f1bf
Andreas Ek <andreas.ek@leasify.se>
2024-05-14 13:04:29 +0200

    wip

--- backup.sh
+++ backup.sh
@@ -4,23 +4,24 @@
 
 #yday=$(date --date "yesterday" +"%Y-%m-%d")
 #echo "${yday}"
+mount /dev/sda2 /mnt/backup
 
-latest=$(ls -td /backup/*/ | head -1)
+latest=$(ls -td /mnt/backup/*/ | head -1)
 
 echo "Latest backup: ${latest}"
 [[ -z "${latest}" ]] && { echo "No latest backup folder found..." ; exit 1; }
 
 now=$(date +"%Y-%m-%d")
-sudo mkdir -p /backup/${now}
-sudo mkdir -p /backup/${now}/leasify-trunk-1
-sudo mkdir -p /backup/${now}/leasify-backups
+sudo mkdir -p /mnt/backup/${now}
+sudo mkdir -p /mnt/backup/${now}/leasify-trunk-1
+sudo mkdir -p /mnt/backup/${now}/leasify-backups
 
 if ! [ "${latest}" = "" ]; then
-    sudo cp -Rf ${latest}leasify-trunk-1 /backup/${now}/leasify-trunk-1
-    sudo cp -Rf ${latest}leasify-backups /backup/${now}/leasify-backups
+    sudo cp -Rf ${latest}leasify-trunk-1 /mnt/backup/${now}/leasify-trunk-1
+    sudo cp -Rf ${latest}leasify-backups /mnt/backup/${now}/leasify-backups
 fi
 
-sudo aws s3 sync s3://leasify-trunk-1 /backup/${now}/leasify-trunk-1
-sudo aws s3 sync s3://leasify-backups /backup/${now}/leasify-backup
+sudo aws s3 sync s3://leasify-trunk-1 /mnt/backup/${now}/leasify-trunk-1
+sudo aws s3 sync s3://leasify-backups /mnt/backup/${now}/leasify-backup
 
-sudo find /backup -type d -mtime +30 -exec sudo rm -rf {} \;
+sudo find /mnt/backup -type d -mtime +30 -exec sudo rm -rf {} \;
